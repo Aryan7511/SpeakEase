aa959ee16f85dda14fc1f5a65ce1aa2a24c6b072
aryan <comauro7511@gmail.com>
2024-07-11 06:23:18 +0530

    changes in css

--- src/components/Inputs/TextArea.jsx
+++ src/components/Inputs/TextArea.jsx
@@ -5,7 +5,7 @@ const TextArea = ({ id, value, onChange, placeholder }) => (
     rows={5}
     id={id}
     className="py-2.5 px-4 border-none focus:outline-none block w-full border-transparent 
-    rounded-lg dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400"
+    rounded-lg dark:bg-neutral-900 dark:border-transparent dark:text-neutral-400 min-h-[160px]"
     placeholder={placeholder}
     value={value}
     onChange={onChange}
